b0789138585c8f7643a26a377d04016f15a6c151
Monserrate Riera <mrieramar@uoc.edu>
2024-07-15 16:50:34 +0200

    chore: Add login and logout functionality to LoginPage and Navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () =>{
+        navigate('/login', {
+            replace: true
+          });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -52,7 +61,9 @@ export const Navbar = () => {
                     <span className='nav-item nav-link text-primary'>
                         Monx
                     </span>
-                    <button className='nav-item nav-link btn'>
+                    <button className='nav-item nav-link btn'
+                        onClick={onLogout}
+                    >
                         Logout
                     </button>
                 </ul>
