6b1a6c9491f36d5ade760232abee0aa49f29d2aa
Monserrate Riera <mrieramar@uoc.edu>
2024-07-18 10:19:25 +0200

    added SearchPage

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,11 +1,38 @@
 import React from "react";
-
+import { HeroCard } from "../components/";
 export const SearchPage = () => {
+
+
+  
   return (
-    <div>
-      <h1>Search</h1>
-    </div>
+    <>
+      <h1>Search Page</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form action="">
+            <input
+              type="text"
+              name="searchText"
+              placeholder="Search a hero"
+              className="form-control"
+              autoComplete="off"
+            />
+            <button className="btn btn-outline-primary mt-1">Search</button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          <div className="alert alert-primary">Search a hero</div>
+          <div className="alert alert-danger">
+            There's no Hero with <b>ABC</b>
+          </div>
+          <HeroCard />
+        </div>
+      </div>
+    </>
   );
 };
-
-
