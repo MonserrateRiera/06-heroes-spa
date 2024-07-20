3cdc5aeb5cfba4d5d15277488814065f6542d31d
Monserrate Riera <mrieramar@uoc.edu>
2024-07-20 10:37:33 +0200

    refactor: Update routing in AppRouter component
    The code changes include:
    - Adding PrivateRoute and PublicRoute components to handle authenticated and public routes respectively.
    - Updating the routing in the AppRouter component to use the new PrivateRoute and PublicRoute components.
    
    This commit refactors the routing in the AppRouter component to improve authentication functionality and handle different types of routes.

---
+++ src/router/PublicRoute.jsx
@@ -0,0 +1,15 @@
+
+import React, { useContext } from 'react';
+import { AuthContext } from '../auth';
+import { Navigate } from 'react-router-dom';
+
+const PublicRoute = ({ children }) => {
+
+    const { logged } = useContext(AuthContext);
+
+  return (logged)
+    ? <Navigate to='/marvel'/>
+    : children
+};
+
+export default PublicRoute;
